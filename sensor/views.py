0dace28f34f77390d41d8c11b8a1306579eb566a
Hary <agus.hary16@gmail.com>
2019-01-22 18:56:23 +0800

    Commit Django

--- sensor/views.py
+++ sensor/views.py
@@ -4,23 +4,21 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+
 # Create your views here.
 
 
 class sensorCrud():
 
-
     @api_view(['GET'])
     def uploadData(request):
-
         if request.method == 'GET':
             insertSensorData = sensorData.objects.create()
             insertSensorData.nodeKey = request.GET['nodeKey']
             insertSensorData.flowData = request.GET['flowData']
             insertSensorData.paData = request.GET['paData']
 
+            insertSensorData.save()
 
-            if(insertSensorData.save()):
-                return HttpResponse(status=status.HTTP_200_OK)
-            else:
-                return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+            return render(request, 'success.html', status=200)
